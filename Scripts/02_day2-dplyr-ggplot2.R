9a143e592c9d433da3270def4487aba2f11c3285
Anna <ania.posacka@gmail.com>
2018-03-02 12:47:37 -0800

    script day 2

--- Scripts/02_day2-dplyr-ggplot2.R
+++ Scripts/02_day2-dplyr-ggplot2.R
@@ -7,7 +7,7 @@
 
 # Recommended workflow for running analysis in R
 
-# 1) start a new R Porject (File>New Project) or open your existing project
+# 1) start a new R Project (File>New Project) or open your existing project
 # 2) start fresh, clean workspace and re-start R often: Session > R restart (clears any libraries
 #   previosuly installed, good practice)
 # 3) Install any packages you need : install.packages("package_name", dependancies = TRUE)
@@ -25,8 +25,9 @@
 library(tidyverse) # has dplyr and ggplot, 
 library(gapminder) # dataset we are going to be working with
 
-# let's create an object for 
-dat <- gapminder
+# let's create an object in our workspace. Remember that putting things
+# into brackets will print it in the console
+(dat <- gapminder)
 
 # if you were to import your own data (as csv) you would use"
 # dat <- read_csv("Data/my_data.csv")
@@ -56,7 +57,6 @@ levels(dat$continent) # lists continent names
 nlevels(dat$country) # tells me how many countries there are
 
 
-
 #------------------------------------------------------------------------------------------------
 # let's mine this data for interesting information with the dply package
 # 
@@ -77,6 +77,7 @@ nlevels(dat$country) # tells me how many countries there are
 # Let's say you are interested in data for Canada in year 1985
 # filter(data_name,...)
 
+# 
 filter(dat, country == "Canada", year < 1985)
 filter(dat, country == "Canada", year > 2000)
 
@@ -84,5 +85,175 @@ filter(dat, country == "Canada", year > 2000)
 
 filter(dat, country == "Canada", year < 1985 & year > 2000)
 
-dat%>%filter(continent == "Europe")
+# what about specific countries
+filter(dat, country %in% c("Canada", "Poland"), year == 2000)
+
+#subset the data on variables or columns. I typically use this when 
+# I am creating a new dataframe
+select(dat, country, lifeExp)
+
+
+#-------------------PIPE--------------------------------------------
+# PIPE
+# working with the pipe operator
+
+dat%>%filter(country %in% c("Canada", "Poland"), year == 2007)
+
+# using the pipe let's us run multiple commands, without retyping
+dat %>%
+  filter(country == "Cambodia") %>%
+  select(year, lifeExp)
+
+# let's add some variables, create a new object, say you needed to focus
+# on only the Americas and calculate the gdp
+
+levels(dat$continent)
+
+#------------------MUTATE----------------------------------------------
+# BEFORE YOU ASSIGN RUN it and check
+i_need_to_know_gdp <- dat%>%
+  filter(continent == "Americas")%>%
+  mutate(gdp = pop * gdpPercap)
+
+summary(i_need_to_know_gdp$gdp)
+
+# nlevels(i_need_to_know_gdp$continent)
+# an example of when this is useful: normalizing chl concentration to  volume filtered
+
+#-----------------ARRANGE---------------------------------------------
+# Use arrange() to row-order data in a principled way
+
+# WE'RE BUILDING ON THIS BEFORE WE MOVE TO GRAPHS/ EDA
+# say you'd like to arrange your data by year, then country
+dat %>%
+  arrange(year, country)
+
+dat %>%
+  filter(country == "Canada", year > 1950) %>%
+  arrange(lifeExp)
+
+# descending or ascending order for life exp? no problem
+dat %>%
+  filter(year == 2007) %>%
+  arrange(desc(lifeExp))
+
+
+#----------------GROUP_BY-AND-SUMMARIZE-------------------------------
+# imagine
+# YOU have your data and you want to calculate summary statistics
+# for every station or experiment that you performed
+
+# group_by() grouping information needed for computations within the groups.
+
+# summarize() takes a dataset with n observations,
+# computes requested summaries, and returns a dataset with 1 observation.
+
+# we are typically interested in things like, mean, median, min, max, se, sd
+# number of observations
+
+# let's get this info for each continent and save it to a csv file
+se <- function(x){sd(x)/sqrt(length(x))}
+
+sum_stats <- dat %>%
+  group_by(continent) %>%
+  summarize(n = n(),
+            mean_GDP = mean(gdpPercap),
+            median_GDP = median(gdpPercap),
+            stdev_GDP = sd(gdpPercap),
+            se_GDP = se(gdpPercap),
+            min_GDP = min(gdpPercap),
+            max_GDP = max(gdpPercap))
+
+# tidy_carbs <- na.omit(tidy_carbs)
+
+write_csv(sum_stats,"01_summary-statistics-Nature-manuscript-final.csv")
+
+
+#----------------RENAME-------------------------------------------------
+
+dat %>%
+  rename(life_exp = lifeExp,
+         gdp_percap = gdpPercap,
+         gdp_percap_rel = gdpPercapRel)
+
+
+
+
+
+
+#-------------GRAPHS----------------------------------------------------
+
+# The basics
+
+ggplot(dat, aes(x = gdpPercap, y = lifeExp)) # nothing to plot yet!
+
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
+  geom_point()
+
+ggplot(gapminder, aes(x = log10(gdpPercap), y = lifeExp)) +
+  geom_point()
+
+p1 <- dat %>%
+      ggplot(aes(x = log10(gdpPercap), y = lifeExp), color = continent) + 
+      geom_point(aes (color = continent),size = 3)
+ 
+# EXPLORATORY DATA ANALYSIS
+# WHAT IS THE MIN GPD per capita for each country in America
+dat%>%
+  filter(continent == "Americas")%>% # focusing on countries in America
+  group_by(country)%>% # for each country...
+  summarise(min_GDP = min(gdpPercap))%>% # compute the min and max of gdp per cap
+  ggplot(aes(x = min_GDP, y = reorder(country, min_GDP),color = country))+ # for info on reorder ( ) read below
+  geom_point(size = 3, stroke=1.25)+
+  theme_bw()+
+  theme(legend.position ="none")+ # removes the legend
+  labs(title="GDP per Capita per country in Americas")+
+  xlab("Minimum GDP per Capita")+
+  ylab("Country")
+
+
+
+#-------------------BOXPLOTS--------------------------------------------------
+
+p2 <- dat%>%
+#  filter(Macro %in% c("P:C","N:C","S:C","C:N"))%>%
+  ggplot(aes(x = continent,y = gdpPercap,color = continent))+
+  geom_boxplot()+
+  geom_point(alpha = 1/2)+
+#  geom_jitter()
+#  scale_color_manual(values=c("#00BA38", "#E7B800"))+
+  ylab("GDP per Capita")+
+#  facet_wrap( ~ Macro,nrow=1, scales="free")+
+  theme_bw()
+ 
+   theme( axis.text.x = element_blank(),
+         axis.title.x = element_blank(),
+         legend.title = element_blank(),
+         panel.grid.major.x = element_blank(), 
+         panel.grid.minor.x = element_blank(),
+         panel.grid.major.y = element_blank(),
+         panel.grid.minor.y = element_blank(),
+         axis.title = element_text(size=14),
+         axis.text = element_text(size=14),
+         strip.text.x = element_text(size = 14,face= "bold"),
+         legend.background = element_rect(fill="gray93"),
+         panel.background = element_rect(fill="white"),
+         plot.background  = element_rect(fill="gray93"))
+
+#------------------ERROR-BARS--------------------------------------------------
+
+
+
+
+#------------------MULTIPLE-PLOTS----------------------------------------------
+
+
+
+
+
+#-----------------SAVING-PLOTS
+library(cowplot)
+plot <-plot_grid(p6,p1,p2,p3,p5,p4,align = "v",nrow=6)
+
+save_plot (filename="Cu_effects-metallome.tiff", plot= plot, base_height= 6, base_width=5)
 
